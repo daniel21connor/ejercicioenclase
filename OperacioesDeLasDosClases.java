c6138aae232ded8d2ad8c4d61146c36a3fe65f7d
daniel21connor <jconnorr1@miumg.edu.gt>
2022-04-09 03:06:08 +0200

    se agrego lo de pares  e impares perfecto

--- OperacioesDeLasDosClases.java
+++ OperacioesDeLasDosClases.java
@@ -1,11 +1,13 @@
 
 package ejercicioenclase;
+
 import java.util.Scanner;
 
 
 
 public class OperacioesDeLasDosClases {
 static  Scanner scanner=new Scanner (System.in);
+
    
 @SuppressWarnings("fallthrough")
     public static void main(String[] args) {
@@ -27,30 +29,36 @@ static  Scanner scanner=new Scanner (System.in);
        
       switch(opcion){
                
-         case 1 : 
+         case 1 -> { 
              System.out.print("digite un numero");
              int numer = scanner.nextInt();
-            System.out.println ( metodo.factorial(numer));
-            break ;
+             System.out.println ( metodo.factorial(numer));
+               }
           
-         case 2 :
-            System.out.print ("digite un numeros");
-            int golpe = scanner.nextInt();
-            System.out.println (metodo.tabla(golpe));
-            break;
+         case 2 -> {
+             System.out.print ("digite un numeros");
+             int golpe = scanner.nextInt();
+             System.out.println (metodo.tabla(golpe));
+               }
             
         
-          case 3: 
-         
-         int numeral = scanner.nextInt();
-              System.out.println (objetoDos.numero(numeral));
-              break;
-          case 4 :
-                  System.out.print("escoge el dia de la semana");
-                  int uno= scanner.nextInt();
-                  System.out.print(""+objetoDos.dia(uno));
-    
-     break;  
+          case 3 -> {
+              System.out.println("Digite la cantidad de numero");
+              int cantidad1= scanner.nextInt();
+              System.out.println(objetoDos.numero(10));
+            int Termino = 0;
+               if (Termino<=0){
+                  boolean ecuacion = false;
+              }else{
+                  System.out.println(" ");
+              }  }
+
+      
+          case 4 -> {
+              System.out.print("escoge el dia de la semana");
+              int uno= scanner.nextInt();
+              System.out.print(""+objetoDos.dia(uno));
+               }  
      
          }      } while (opcion !=5);
 
--- SerieDos.java
+++ SerieDos.java
@@ -11,54 +11,45 @@ public class SerieDos {
     
    
     public  int numero (int numero){
-         int arreglo [] = new  int [10];
-        int conteoPares = 0, conteoImpares = 0;
-          
-        
-       for (int i=0; i<10; i++){
-           System.out.print((i+1)+ "   digite numero");
-           arreglo[i]=scanner.nextInt();
-         if(arreglo[i] %  2==0){
-             conteoPares++;
-       
-         }
-         else 
-             conteoImpares ++;
-          
+        int cantidadPar=0, cantidadImpar=0, Promedio, Mayores=0, Termino, Acumulacion=0, sumaPares=0, sumaImpares=0;
+         int cantidad1= scanner.nextInt();
+         boolean ecuacion = true;
          
-       }
-       int par[]= new int [conteoPares];
-       int impar[]=new int [conteoImpares];
-       
-       conteoPares=0;
-       conteoImpares=0;
-       
-       for (int i=0;i<10;i++){
-           if (arreglo[i] % 2==0){
-               par [conteoPares]= arreglo[i];
-               conteoPares ++;
-           }
-        else {
-               impar[conteoImpares]=arreglo[i];
-               conteoImpares ++;
-               }
-
-    }
-       System.out.println ("pares");
-       for (int i=0; i<conteoPares; i++) {
-           System.out.println(par[i]+"-  ");
-           
-       }
-       
-       System.out.println ("impares");
-       for (int i=0; i<conteoImpares; i++) {
-           System.out.print(impar[i]+"-   ");
-       }
-       System.out.println("");
-       return 0;
-    }
-
-        
+         while (ecuacion) {
+             for (int i =1; i<cantidad1; i++){
+                 System.out.println("Digite numero" +i);
+                 int num = scanner.nextInt();
+                 
+                 if(num%2==0){
+                     cantidadPar++;
+                     sumaPares= sumaPares+num;
+                 }else{
+                   cantidadImpar++;                
+                   sumaImpares= sumaImpares+num;
+                 }
+            if(num >=Mayores){
+                Mayores = num;
+            }
+            Acumulacion= sumaImpares+sumaPares;
+            
+            }
+            Promedio= cantidadImpar;
+            
+              System.out.println("Cantidad de numero pares es :" + cantidadPar);
+              System.out.println("Cantidad de numeros impares es :" + cantidadImpar);
+              System.out.println("La suma total es de :" + Acumulacion);
+              System.out.println("El promedio de los numero impares :" + Promedio);
+              System.out.println("0.Salir ");
+              Termino = scanner.nextInt(); 
+              if (Termino<=0){
+              ecuacion = false;
+              }else{
+              System.out.println(" ");
+              }
+            }  
+        return numero;
+  
+      }   
   
     
    
